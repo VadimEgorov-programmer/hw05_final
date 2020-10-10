61701a0e39f2a17d64e811d1cb97a5a20857e35c
Vadim <wadim-egoroff@yandex.ru>
2020-10-11 02:35:46 +0300

    fix posts.urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     # path("follow/", views.follow_index, name="follow_index"),
     path("<str:username>/follow/", views.profile_follow, name="profile_follow"),
     path("<str:username>/unfollow/", views.profile_unfollow, name="profile_unfollow"),
-
 ]
